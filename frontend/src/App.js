318f1a40d3a271dc2ca9a539e6f36f491b5df875
Anatoly Korzakov <tolik_83@mail.ru>
2019-12-03 15:39:43 +0300

    Шаблон v 1.0.1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
 
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Main from './components/Main';
+
 function App() {
   return (
-    <div className="App">
-
+    <div className='App'>
+      <div className='header'>
+        <Header />
+      </div>
+      <div className='underheader'>
+        <div className='menu'>
+          <Menu />
+        </div>
+        <div className='main'>
+          <Main />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Заголовок</h1>
+        </div>
+    )
+}
+
+export default Header;
