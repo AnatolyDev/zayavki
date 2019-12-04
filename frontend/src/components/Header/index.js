5bdba45aea3df5f37bb40b380b4a8f2a2ed17db9
Anatoly <tolik_83@mail.ru>
2019-12-04 23:23:45 +0300

    Заголовок с авторизацией v 1.0.1

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { NavItem } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Заголовок</h1>
+        <div className='header-main'>
+            <div className='header-logo'>
+                Логотип
+            </div>
+            <div className='header-title'>
+                Заголовок
+            </div>
+            <div className='header-signin'>
+                <NavItem>
+                    <NavLink to='/signin' activeClassName='active'>Войти</NavLink>
+                </NavItem>
+            </div>
         </div>
     )
 }
--- frontend/src/components/Main/index.js
+++ frontend/src/components/Main/index.js
@@ -3,13 +3,15 @@ import { Route, Switch } from 'react-router-dom';
 
 import Home from '../Home';
 import Catalog from '../Catalog';
+import SignIn from '../SignIn';
 
 const Main = () => {
     return (
         <>
             <Switch>
-                <Route path='/' exact component={Home} />
+                <Route exact path='/' exact component={Home} />
                 <Route path='/catalog' component={Catalog} />
+                <Route path='/signin' component={SignIn} />
             </Switch>
         </>
     )
