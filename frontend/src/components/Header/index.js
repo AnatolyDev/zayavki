083769671f9dfcefe5845b84d6b98ca4328f25bc
Anatoly <tolik_83@mail.ru>
2019-12-05 23:17:46 +0300

    Заголовок из material-ui

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -5,18 +5,60 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import Switch from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormGroup from '@material-ui/core/FormGroup';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
 
     const [auth, setAuth] = React.useState(true);
     const [anchorEl, setAnchorEl] = React.useState(null);
+    const [state, setState] = React.useState(false);
     const open = Boolean(anchorEl);
+
+    const toggleDrawer = (open) => event => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+          return;
+        }
+    
+        setState(open);
+      };
+
+      const sideList = side => (
+        <div
+          className='menu-list'
+          role="presentation"
+          onClick={toggleDrawer(side, false)}
+          onKeyDown={toggleDrawer(side, false)}
+        >
+          <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      );
   
     const handleChange = event => {
       setAuth(event.target.checked);
@@ -32,9 +74,12 @@ const Header = () => {
 
     return (
         <div className='header-main'>
+            <Drawer open={state} onClose={toggleDrawer(false)}>
+                {sideList('left')}
+            </Drawer>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton edge="start" className='menu-button' color="inherit" aria-label="menu">
+                    <IconButton edge="start" className='menu-button' color="inherit" aria-label="menu" onClick={toggleDrawer('left', true)}>
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" className='header-title'>
